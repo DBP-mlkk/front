1e6c9c582c3fda124463ece657b5dd5b9104056e
Door_km <156165073+MunKyungMan@users.noreply.github.com>
2024-06-10 23:46:51 +0900

    modified:   login.css 	modified:   login.html 	modified:   login.js

--- login.js
+++ login.js
@@ -14,14 +14,34 @@ document.addEventListener("keydown", function(event) {
 function checkLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+    var remember = document.getElementById("remember").checked;
+    var checkboxIcon = document.querySelector(".checkbox-icon");
 
     // 가정상으로, username이 "user"이고 password가 "password"라고 가정
     if (username === "user" && password === "password") {
         // 로그인 성공시 다음 페이지로 이동
         alert("로그인 성공!");
+
+        if (remember) {
+            // TODO: 로그인 정보 저장 처리
+            checkboxIcon.innerHTML = "V";
+        } else {
+            checkboxIcon.innerHTML = "";
+        }
+
         window.location.href = "login_complete.html";
     } else {
         // 로그인 실패시 사용자에게 알림 표시
         alert("사용자 정보가 일치하지 않습니다.");
     }
 }
+
+// "로그인 정보 저장" 체크박스 클릭 이벤트 핸들러
+document.getElementById("remember").addEventListener("click", function() {
+    var checkboxIcon = document.querySelector(".checkbox-icon");
+    if (this.checked) {
+        checkboxIcon.innerHTML = "V";
+    } else {
+        checkboxIcon.innerHTML = "";
+    }
+});
